9fdb409e38cea5bc9b78ac16f85f77ec7aa3b22d
Jesse Davila <jessedavila3@gmail.com>
2016-12-15 16:54:16 -0600

    i give up

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,10 +1,10 @@
+import java.sql.*;
 import java.util.List;
 
 /**
  * Created by jessedavila on 12/15/16.
  */
 public class MySQLAdsDao implements Ads {
-
     @Override
     public List<Ad> all() {
         return null;
@@ -12,6 +12,18 @@ public class MySQLAdsDao implements Ads {
 
     @Override
     public Long insert(Ad ad) {
+        String selectQuery = "SELECT * FROM MOVIES";
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jbdc:mysql://localhost/adlister_db",
+                    "root",
+                    "codeup"
+            );
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(selectQuery);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
