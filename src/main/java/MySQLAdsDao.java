4694f9fd2d2a90b09fc5029f1f453441c5acef92
Jesse Davila <jessedavila3@gmail.com>
2016-12-15 15:59:51 -0600

    putting it all together with jdbc stuff

---
+++ src/main/java/MySQLAdsDao.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+/**
+ * Created by jessedavila on 12/15/16.
+ */
+public class MySQLAdsDao implements Ads {
+
+    @Override
+    public List<Ad> all() {
+        return null;
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        return null;
+    }
+}
